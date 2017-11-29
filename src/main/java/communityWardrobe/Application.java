6dab99c237cf345bc688b3bfecf1e627bd431131
Shiv Khattar <skhattar@akamai.com>
2017-11-29 14:30:42 +0530

    no message

--- src/main/java/communityWardrobe/Application.java
+++ src/main/java/communityWardrobe/Application.java
@@ -1,7 +1,12 @@
 package communityWardrobe;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -13,4 +18,20 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+        @Bean
+        public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+            return args -> {
+
+                System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+                String[] beanNames = ctx.getBeanDefinitionNames();
+                Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    System.out.println(beanName);
+                }
+
+            };
+        }
+    
 }
